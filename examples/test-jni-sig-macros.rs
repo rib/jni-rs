50b2d00bb837ef132b49fa5429cfe38aebd79f47
Robert Bragg <robert@sixbynine.org>
2025-09-30 01:20:38 +0100

    stash

--- examples/test-jni-sig-macros.rs
+++ examples/test-jni-sig-macros.rs
@@ -1711,8 +1711,8 @@ fn main() {
     let sig = print_jni_sig_for!((a: jint, b: [java.lang.String]) -> java.lang.String[][]);
     assert_eq!(sig, "(I[Ljava/lang/String;)[[Ljava/lang/String;\0");
 
-    let sig = print_jni_sig_for!((a: jint, b: java.lang.String[]) -> java.lang.String[][]);
-    assert_eq!(sig, "(I[Ljava/lang/String;)[[Ljava/lang/String;\0");
+    let sig = print_jni_sig_for!((a: jint, b: java.lang.String[]) -> java.lang.String);
+    assert_eq!(sig, "(I[Ljava/lang/String;)Ljava/lang/String;\0");
 
     println!("\n=== Testing Individual Descriptors ===");
     // Test that primitive arrays now parse correctly
